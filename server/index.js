8257294a4ab17b99cb95f94aca3e90db326aec22
Bharathkumar <kunkibharathkumar0428@gmail.com>
2025-08-21 12:06:41 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
   {
-    origin : ["https://deploy-mern-1whq.vercel.app"],
+    origin : ["https://elite-walk-frontend.vercel.app"],
     methods : ["POST","GET"],
     credentials : true
 ));
